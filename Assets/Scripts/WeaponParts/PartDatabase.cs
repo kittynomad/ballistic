cc3a36fc095645c080ad97ad5555894d6ea20abb
PierceNunn <agentp5151@gmail.com>
2026-02-18 11:17:44 -0600

    new weapon parts

--- Assets/Scripts/WeaponParts/PartDatabase.cs
+++ Assets/Scripts/WeaponParts/PartDatabase.cs
@@ -7,8 +7,12 @@ public class PartDatabase
     [SerializeField] private List<WeaponFrame> _weaponFrames;
     [SerializeField] private List<WeaponBattery> _weaponBatteries;
     [SerializeField] private List<WeaponMagazine> _weaponMagazines;
+    [SerializeField] private List<WeaponMuzzle> _weaponMuzzles;
+    [SerializeField] private List<WeaponAddon> _weaponAddons;
 
     public List<WeaponFrame> WeaponFrames { get => _weaponFrames; set => _weaponFrames = value; }
     public List<WeaponBattery> WeaponBatteries { get => _weaponBatteries; set => _weaponBatteries = value; }
     public List<WeaponMagazine> WeaponMagazines { get => _weaponMagazines; set => _weaponMagazines = value; }
+    public List<WeaponMuzzle> WeaponMuzzles { get => _weaponMuzzles; set => _weaponMuzzles = value; }
+    public List<WeaponAddon> WeaponAddons { get => _weaponAddons; set => _weaponAddons = value; }
 }
