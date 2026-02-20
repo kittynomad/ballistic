45ff0fac5ffce8795a1752b57ea1eba4edba7bd9
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-02-20 11:44:55 -0600

    added more modifiers

--- Assets/Scripts/DataStuff/Enums.cs
+++ Assets/Scripts/DataStuff/Enums.cs
@@ -4,8 +4,11 @@ public class Enums
 {
     public enum Modifiers
     {
-        fire,
-        electricity,
+        fire = 0,
+        electricity = 1,
+        multishot = 2,
+        spread = 3,
+        healing = 4,
     }
 
     public enum Operators
