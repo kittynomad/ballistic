8db81ea4eb4413339baae96322b91bae6f87a6b7
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-02-27 14:57:27 -0600

    more model controller stuff

--- Assets/Scripts/WeaponParts/ModelScripts/FrameModelController.cs
+++ Assets/Scripts/WeaponParts/ModelScripts/FrameModelController.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 using NaughtyAttributes;
 
 public class FrameModelController : MonoBehaviour
@@ -6,14 +7,38 @@ public class FrameModelController : MonoBehaviour
     [SerializeField] private GameObject _batteryConnectionPoint;
     [SerializeField] private GameObject _magazineConnectionPoint;
     [SerializeField] private GameObject _muzzleConnectionPoint;
+    [SerializeField] private GameObject _backConnectionPoint;
     [SerializeField] private GameObject[] _modifierConnectionPoints;
 
-    [SerializeField] private PartModelController _testMuzzle;
+    private List<PartModelController> attachedParts = new List<PartModelController>();
 
+    [SerializeField] private PartModelController _testMuzzle;
     public void ConnectPart(PartModelController part, Vector3 connectionPoint)
     {
         part.transform.position = connectionPoint + part.PartConnectionPoint.transform.localPosition;
         part.transform.parent = transform;
+        attachedParts.Add(part);
+    }
+
+    public void ConnectPart(PartModelController part, GameObject connectionPoint)
+    {
+        ConnectPart(part, connectionPoint.transform.position);
+    }
+
+    public void RemovePart(PartModelController part, bool destroyPart = true)
+    {
+        if(attachedParts.Contains(part))
+        {
+            attachedParts.Remove(part);
+            part.transform.parent = null;
+            if (destroyPart) Destroy(part.gameObject);
+        }
+    }
+
+    [Button]
+    public void TestRemovePart()
+    {
+        RemovePart(_testMuzzle, true);
     }
 
     [Button]
