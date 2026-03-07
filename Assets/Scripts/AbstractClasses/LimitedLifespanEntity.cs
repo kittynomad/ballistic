084881492296e363c99f42f19086c175be98c9eb
PierceNunn <agentp5151@gmail.com>
2026-03-07 17:04:38 -0600

    created LimitedLifespanEntity.cs

---
+++ Assets/Scripts/AbstractClasses/LimitedLifespanEntity.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+
+public abstract class LimitedLifespanEntity : MonoBehaviour
+{
+    [SerializeField] private float _totalLifespan;
+    private float currentLifespan;
+
+    public float TotalLifespan { get => _totalLifespan; set => _totalLifespan = value; }
+    public float CurrentLifespan { get => currentLifespan; set => currentLifespan = value; }
+
+    //in case inheriting scripts want to do a little something extra
+    public virtual void StartLifeTimer()
+    {
+        StartCoroutine(CountDownLife());
+    }
+
+    public IEnumerator CountDownLife()
+    {
+        //looping in fixed update so lifespan can be tracked (idk if i'll actually use this)
+        currentLifespan = _totalLifespan;
+        while(currentLifespan > 0f)
+        {
+            currentLifespan -= Time.deltaTime;
+            yield return new WaitForFixedUpdate();
+        }
+        Destroy(gameObject);
+    }
+}
