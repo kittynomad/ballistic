38c463b95c724f7df6b80d06aff06dce146f2b34
PierceNunn <agentp5151@gmail.com>
2026-03-23 11:51:05 -0500

    set up gameplay gun preview

--- Assets/Scripts/Managers/CameraManager.cs
+++ Assets/Scripts/Managers/CameraManager.cs
@@ -9,13 +9,29 @@ using UnityEngine;
 
 public class CameraManager : Manager
 {
+    [SerializeField] private Vector3[] assemblyCameraTransformVars;
+    [SerializeField] private Vector3[] assembledCameraTransformVars;
     private Camera assemblyPreviewCamera;
     private Vector3 assemblyCameraOffset = new Vector3(6f, 0f, 0f);
     public override async Awaitable Initialize()
     {
         GameObject temp = Instantiate(Resources.Load("UI/AssemblyPreviewCamera") as GameObject);
         assemblyPreviewCamera = temp.GetComponent<Camera>();
-        temp.transform.position = AssemblyUIService.VIEWMODEL_POSITION + assemblyCameraOffset;
+        SetAssemblyModeCamera();
+        SetGameplayModeCamera();
         await base.Initialize();
+    }
+
+    public void SetAssemblyModeCamera()
+    {
+        GameObject temp = assemblyPreviewCamera.gameObject;
+        temp.transform.position = AssemblyUIService.VIEWMODEL_POSITION + assemblyCameraTransformVars[0];
+        temp.transform.localEulerAngles = assemblyCameraTransformVars[1];
+    }
+    public void SetGameplayModeCamera()
+    {
+        GameObject temp = assemblyPreviewCamera.gameObject;
+        temp.transform.position = AssemblyUIService.VIEWMODEL_POSITION + assembledCameraTransformVars[0];
+        temp.transform.localEulerAngles = assembledCameraTransformVars[1];
     }
 }
