e19c549e9367662c3c24c3b8c4d4f368e265abca
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-03-29 12:40:51 -0500

    map is now spawned by MapLoaderService

--- Assets/Scripts/Services/GameplayServicesScripts/MapLoaderService.cs
+++ Assets/Scripts/Services/GameplayServicesScripts/MapLoaderService.cs
@@ -2,5 +2,11 @@ using UnityEngine;
 
 public class MapLoaderService : Service
 {
-    
+    [SerializeField] private GameObject _map;
+
+    public override Awaitable Initialize()
+    {
+        Instantiate(_map);
+        return base.Initialize();
+    }
 }
