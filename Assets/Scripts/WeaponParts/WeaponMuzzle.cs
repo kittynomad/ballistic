cc2b10a217b6e80b1bd6c1f3cc3a9b12f9f2bbca
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-04-02 08:45:43 -0500

    removed ModifierDef in WeaponMuzzle (just therte for testing)

--- Assets/Scripts/WeaponParts/WeaponMuzzle.cs
+++ Assets/Scripts/WeaponParts/WeaponMuzzle.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 public class WeaponMuzzle : WeaponPart
 {
     [SerializeField] private float _spread;
-    [SerializeReference, AbstractSerializer] private ModifierDef mod;
 
     public float Spread { get => _spread; set => _spread = value; }
 }
