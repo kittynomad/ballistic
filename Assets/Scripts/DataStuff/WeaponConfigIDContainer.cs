d2dd5155243bd530b7d5f77e6d4a67bd5d3f9e5b
PierceNunn <agentp5151@gmail.com>
2026-04-30 13:50:15 -0500

    made weapon to id container function

--- Assets/Scripts/DataStuff/WeaponConfigIDContainer.cs
+++ Assets/Scripts/DataStuff/WeaponConfigIDContainer.cs
@@ -8,4 +8,27 @@ public class WeaponConfigIDContainer
     [SerializeField] private string _magazineID;
     [SerializeField] private string _muzzleID;
     [SerializeField] private string[] _addonIDs;
+
+    public string FrameID { get => _frameID; set => _frameID = value; }
+    public string BatteryID { get => _batteryID; set => _batteryID = value; }
+    public string MagazineID { get => _magazineID; set => _magazineID = value; }
+    public string MuzzleID { get => _muzzleID; set => _muzzleID = value; }
+    public string[] AddonIDs { get => _addonIDs; set => _addonIDs = value; }
+
+    public static WeaponConfigIDContainer WeaponToIDContainer(WeaponConfig wc)
+    {
+        WeaponConfigIDContainer output = new WeaponConfigIDContainer();
+        output.FrameID = wc.Frame.Id;
+        output.BatteryID = wc.Battery.Id;
+        output.MagazineID = wc.Magazine.Id;
+        output.MuzzleID = wc.Muzzle.Id;
+        output.AddonIDs = new string[wc.Addons.Length];
+
+        for(int i = 0; i < wc.Addons.Length; i++)
+        {
+            output.AddonIDs[i] = wc.Addons[i].Id;
+        }
+
+        return output;
+    }
 }
