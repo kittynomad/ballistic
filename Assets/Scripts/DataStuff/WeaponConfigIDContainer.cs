4b83d588f9b5c535da972cf34d685301675fa5b5
PierceNunn <agentp5151@gmail.com>
2026-04-30 13:32:31 -0500

    created WeaponConfigIDContainer

---
+++ Assets/Scripts/DataStuff/WeaponConfigIDContainer.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public struct WeaponConfigIDContainer
+{
+    [SerializeField] private string _frameID;
+    [SerializeField] private string _batteryID;
+    [SerializeField] private string _magazineID;
+    [SerializeField] private string _muzzleID;
+    [SerializeField] private string[] _addonIDs;
+}
