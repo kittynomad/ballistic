3e449596a29e362f09b33109c6304348fcf655b2
kittynomad <59345740+kittynomad@users.noreply.github.com>
2026-05-07 10:29:38 -0500

    loadoutUI now displays loadout parts

--- Assets/Scripts/UI/LoadoutViewManager.cs
+++ Assets/Scripts/UI/LoadoutViewManager.cs
@@ -5,6 +5,9 @@ using UnityEngine.UI;
 
 public class LoadoutViewManager : MonoBehaviour
 {
+    [SerializeField] private TextMeshProUGUI _loadoutNameDisplay;
+    [SerializeField] private TextMeshProUGUI _loadoutDescriptionDisplay;
+
     private int loadoutIndex = 0;
     private WeaponConfig wc;
     public void InitializeLoadoutDisplay(int index)
@@ -13,7 +16,8 @@ public class LoadoutViewManager : MonoBehaviour
         WeaponConfigIDContainer wcID = ComponentDataService.Instance.WeaponLoadouts.WeaponConfigs[loadoutIndex];
         wc = WeaponConfigIDContainer.IDContainerToWeaponConfig(wcID);
 
-
+        _loadoutNameDisplay.text = "Loadout " + (loadoutIndex + 1);
+        _loadoutDescriptionDisplay.text = wc.ToString();
     }
 
     public void EquipLoadout()
